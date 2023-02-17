720b739b6d918a5bb89d5ddcec53a215834d4886
husseinmo96 <95226088+husseinmo96@users.noreply.github.com>
2023-02-17 23:10:43 +0200

    implement interface

--- Program.cs
+++ Program.cs
@@ -12,7 +12,7 @@ namespace SolidDemo.OCP
     {
 
 
-      ManagerEmployee Emp1 = new ManagerEmployee();
+      IEmployee Emp1 = new ManagerEmployee();
       Emp1.ID = "1";
       Emp1.Name = "Hussein";
       Emp1.BasicSalary = 40000;
@@ -21,7 +21,7 @@ namespace SolidDemo.OCP
         + "Bonus:" + Emp1.calHoursBonus(5).ToString()
       );
 
-      RegularEmployee Emp2 = new RegularEmployee();
+      IEmployee Emp2 = new RegularEmployee();
       Emp2.ID = "2";
       Emp2.Name = "Ahmed";
       Emp2.BasicSalary = 40000;
