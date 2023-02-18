2c544a8102b5ebb7d4f5f860c827490f042ff7a1
husseinmo96 <95226088+husseinmo96@users.noreply.github.com>
2023-02-18 20:41:13 +0200

    ISP - multiple payment methods for same order

--- ISP/IOrder.cs
+++ ISP/IOrder.cs
@@ -1,14 +1,17 @@
 namespace SolidDemo.ISP
 {
-  public interface IOrder
+
+  public interface ICashOrder { void AddCashOrder(); void Complete(); }
+
+  public interface IOnlinePaymentOrder { void AddOnlinePaymentOrder(); }
+
+  public interface ICreditOrder { void AddCreditOrder(); }
+
+
+
+  // When user wants to pay order with multiple payment methods, we can use this interface
+  public interface IOrder : ICashOrder, IOnlinePaymentOrder, ICreditOrder
   {
 
   }
-
-  public interface ICashOrder : IOrder { void AddCashOrder(); void Complete(); }
-
-  public interface IOnlinePaymentOrder : IOrder { void AddOnlinePaymentOrder(); }
-
-  public interface ICreditOrder : IOrder { void AddCreditOrder(); }
-
 }
