5bd338ce2fc59f44ac22ddf7a1472ce5103cd8b0
husseinmo96 <95226088+husseinmo96@users.noreply.github.com>
2023-02-18 21:06:03 +0200

    ISP

---
+++ ISP/Order.cs
@@ -0,0 +1,25 @@
+namespace SolidDemo.ISP
+{
+  public class Order : IOrder
+  {
+    public void AddCashOrder()
+    {
+      // add cash order
+    }
+
+    public void AddCreditOrder()
+    {
+      // add credit order
+    }
+
+    public void AddOnlinePaymentOrder()
+    {
+      // add online payment order
+    }
+
+    public void Complete()
+    {
+      // complete order
+    }
+  }
+}
