a2769b81d39a6eec3c284904cb6d0927d5aa74c7
husseinmo96 <95226088+husseinmo96@users.noreply.github.com>
2023-02-20 00:49:46 +0200

    violate DIP

--- DIP/Notification.cs
+++ DIP/Notification.cs
@@ -3,14 +3,24 @@ namespace DIP
   public class Notification
   {
 
-
+    private Gmail _gmail = new Gmail();
+    private HotMail _hotMail = new HotMail();
 
     public string? address { get; set; }
     public string? message { get; set; }
 
-    public void Send()
+    public string sendGmail()
     {
-      // send notification
+      _gmail.address = address;
+      _gmail.message = message;
+      return _gmail.Send();
+    }
+
+    public string sendHotMail()
+    {
+      _hotMail.address = address;
+      _hotMail.message = message;
+      return _hotMail.Send();
     }
   }
 }
--- Program.cs
+++ Program.cs
@@ -1,11 +1,12 @@
-﻿
-
-public class Program
+﻿public class Program
 {
   public static void Main(string[] args)
   {
 
     DIP.Notification notification = new DIP.Notification();
-
+    notification.address = "hussein@gmail.com";
+    notification.message = "Hello World";
+    Console.WriteLine(notification.sendGmail());
+    Console.WriteLine(notification.sendHotMail());
   }
 }
